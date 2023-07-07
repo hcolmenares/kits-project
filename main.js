8d09edb75696309a220531d068fc2187dfebd3d4
hcolmenares <hcolmenares@corrosioncic.com>
2023-07-07 12:31:32 -0500

    Se agrega navmenu

--- main.js
+++ main.js
@@ -86,3 +86,12 @@ cards.forEach((card) => {
   });
 });
 }
+
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('.header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
